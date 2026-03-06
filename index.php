50b02ceb91b6daa3f6957bd1f69616236f0f55d5
Jabbar Sipra <prowebmaster477@gmail.com>
2026-03-06 12:22:05 -0600

    header section and inti

---
+++ index.php
@@ -0,0 +1,151 @@
+<?php
+
+get_header();
+
+
+
+?>
+
+
+
+<div class="header">
+    <div class="text">
+        <h1>Hi, I'm Jabbar, a Self Taught Software <span class="dev">Developer</span></h1>
+        <p>Based in Pakistan, Specialized in 
+            <span class="dev">Front-end</span>
+             & 
+             <span class="dev">Back-end</span>
+              development.
+        </p>
+        <div class="button-container">
+            <a href="#contact" class="animated-border">
+                Say Hi <span><i class="fa-solid fa-paper-plane"></i></span>
+            </a>
+            <a href="#" class="animated-border">
+                Resume <span><i class="fa-solid fa-download"></i></span>
+            </a>
+        </div>
+        
+
+    </div>
+    <div class="image-box">
+        <div class=" code-box order-1 lg:order-2 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border ">
+            <div class="flex flex-row">
+                <div class="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600">
+
+                </div>
+                <div class="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent">
+
+                </div>
+            </div>
+            <div class="px-4 lg:px-8 py-5">
+                <div class="flex flex-row space-x-2">
+                    <div class="h-3 w-3 rounded-full bg-red-400">
+
+                    </div>
+                    <div class="h-3 w-3 rounded-full bg-orange-400">
+
+                    </div>
+                    <div class="h-3 w-3 rounded-full bg-green-200">
+
+                    </div>
+                </div>
+            </div>
+            <div class="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
+                <code class="font-mono text-xs md:text-sm lg:text-base">
+                    <div class="blink">
+                        <span class="mr-2 text-pink-500">const</span>
+                        <span class="mr-2 text-white">coder</span>
+                        <span class="mr-2 text-pink-500">=</span>
+                        <span class="text-gray-400">{</span>
+                        </div>
+                        <div>
+                            <span class="ml-4 lg:ml-8 mr-2 text-white">name:</span>
+                            <span class="text-gray-400">'</span>
+                            <span class="text-amber-300">Jabbar Azam</span>
+                            <span class="text-gray-400">',</span>
+                        </div>
+                        <div class="ml-4 lg:ml-8 mr-2">
+                            <span class=" text-white">skills:</span>
+                            <span class="text-gray-400">['</span>
+                            <span class="text-amber-300">React</span>
+                            <span class="text-gray-400">', '</span>
+                            <span class="text-amber-300">NextJS</span>
+                            <span class="text-gray-400">', '</span>
+                            <span class="text-amber-300">Redux</span>
+                            <span class="text-gray-400">', '</span>
+                            <span class="text-amber-300">Express</span>
+                            <span class="text-gray-400">', '</span>
+                            <span class="text-amber-300">NestJS</span>
+                            <span class="text-gray-400">', '</span>
+                            <span class="text-amber-300">MySql</span>
+                            <span class="text-gray-400">', '</span>
+                            <span class="text-amber-300">MongoDB</span>
+                            <span class="text-gray-400">', '</span>
+                            <span class="text-amber-300">Docker</span>
+                            <span class="text-gray-400">', '</span>
+                            <span class="text-amber-300">AWS</span>
+                            <span class="text-gray-400">'],</span>
+                        </div>
+                        <div>
+                            <span class="ml-4 lg:ml-8 mr-2 text-white">hardWorker:</span>
+                            <span class="text-orange-400">true</span>
+                            <span class="text-gray-400">,</span>
+                        </div>
+                        <div>
+                            <span class="ml-4 lg:ml-8 mr-2 text-white">quickLearner:</span>
+                            <span class="text-orange-400">true</span>
+                            <span class="text-gray-400">,</span>
+                        </div>
+                        <div>
+                            <span class="ml-4 lg:ml-8 mr-2 text-white">problemSolver:</span>
+                            <span class="text-orange-400">true</span>
+                            <span class="text-gray-400">,</span>
+                        </div>
+                        <div>
+                            <span class="ml-4 lg:ml-8 mr-2 text-green-400">hireable:</span>
+                            <span class="text-orange-400">function</span>
+                            <span class="text-gray-400">() {</span>
+                            </div>
+                            <div>
+                                <span class="ml-8 lg:ml-16 mr-2 text-orange-400">return</span>
+                                <span class="text-gray-400">(</span>
+                            </div>
+                            <div>
+                                <span class="ml-12 lg:ml-24 text-cyan-400">this.</span>
+                                <span class="mr-2 text-white">hardWorker</span>
+                                <span class="text-amber-300">&amp;&amp;</span>
+                            </div>
+                            <div>
+                                <span class="ml-12 lg:ml-24 text-cyan-400">this.</span>
+                                <span class="mr-2 text-white">problemSolver</span>
+                                <span class="text-amber-300">&amp;&amp;</span>
+                            </div>
+                            <div>
+                                <span class="ml-12 lg:ml-24 text-cyan-400">this.</span>
+                                <span class="mr-2 text-white">skills.length</span>
+                                <span class="mr-2 text-amber-300">&gt;=</span>
+                                <span class="text-orange-400">5</span>
+                            </div>
+                            <div>
+                                <span class="ml-8 lg:ml-16 mr-2 text-gray-400">);</span>
+                            </div>
+                            <div>
+                                <span class="ml-4 lg:ml-8 text-gray-400">};</span>
+                            </div>
+                            <div>
+                                <span class="text-gray-400">};</span>
+                            </div>
+                        </code>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php
+
+get_footer();
+?>
